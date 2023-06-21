940302d826c94b4fbc7a58bed3de3cd2616820b7
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-06-21 16:43:08 +0300

    Java request

--- src/main/java/com/orcrist/user/UserApplication.java
+++ src/main/java/com/orcrist/user/UserApplication.java
@@ -1,13 +1,27 @@
 package com.orcrist.user;
 
+import com.orcrist.user.dto.UserDTO;
+import com.orcrist.user.model.User;
+import com.orcrist.user.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return (args) -> {
+			UserDTO user = new UserDTO();
+			user.setUsername("user1");
+			userService.addUser(user);
+		};
 	}
 
 }
